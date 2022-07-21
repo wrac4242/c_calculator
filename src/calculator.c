584a362419c6bf1cd276adb3d4ff717be52374db
Wrac4242 <wrac4242@gmail.com>
2022-07-21 10:16:18 +0100

    add basic functionality

--- src/calculator.c
+++ src/calculator.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdio_ext.h>
+
+struct node  {
+    int value;
+    struct node* next;
+};
+
+typedef struct node node_t;
+
+/* 
+    TODO:
+    basic calculator
+    stack setup
+    polish calculator 
+
+    stack just adds stuff to the head
+*/
+
 
 int main() {
-    printf("hello world\n");
+    float a, b;
+    printf("Please Enter your two floats in the format 'a b': ");
+    scanf("%f %f", &a, &b);
+    printf("add+, sub-, mul*, div/: ");
+
+    char input[50];
+    __fpurge(stdin);
+    fgets(input, sizeof(input), stdin);
+
+    if (strcmp(input, "add")) {
+        printf("%f", a+b);
+    } else if (strcmp(input, "sub")) {
+        printf("%f", a-b);
+    } else if (strcmp(input, "mul")) {
+        printf("%f", a*b);
+    } else if (strcmp(input, "div")) {
+        printf("%f", a/b);
+    } else {
+        printf("not valid");
+    }
+    
     return 0;
 }
